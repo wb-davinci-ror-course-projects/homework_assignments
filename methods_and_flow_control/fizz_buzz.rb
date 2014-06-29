889ec2669f4724809a0ec97d038779a4c7151cd2
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-06-28 18:48:51 -0600

    Remove comments
      * deleted the instructions I orginally put in the
    	program as comments

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,8 +1,3 @@
-# This program should output the numbers 1 through 100, however:
-#   If evenly divisible by 3, print Fizz
-# If evenly divisible by 5, print Buzz
-# If evenly divisible by 3 and 5, print FizzBuzz
-# Otherwise, print out the number
 number = 0
 
 while number >= 0 && number < 100
