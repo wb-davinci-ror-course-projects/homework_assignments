05d422ab4de3b0fcf126d4e250cfd7dd4deeaf69
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-06-30 10:11:44 -0600

    Correct capitialization mistake
      * Changed "this is a 'string'..." to" This is a
    	 'string'..." by using .capitialize method

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -6,7 +6,7 @@ practice = practice_string.slice('practice')
 puts practice_string
 puts practice_string.capitalize
 puts practice_string.upcase
-puts practice_string.gsub('string', "'string'")
+puts practice_string.gsub('string', "'string'").capitalize
 puts "The string '#{practice_string}'" \
        " has #{practice_string.length} characters"
 puts practice_string.reverse
