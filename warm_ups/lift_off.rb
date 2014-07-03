be430acb0805a90b4e44f8c9e8023907a9cbb14a
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-07-02 19:22:20 -0600

    added start variable after class discussion

--- warm_ups/lift_off.rb
+++ warm_ups/lift_off.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
-count = 11
-puts 'T-'
-while count < 12 && count > 1
+
+start_number = 10 #added after class discussion
+count = start_number
+puts "T-#{start_number}"
+while count < start_number + 1 && count > 1
   count -= 1
   puts count
 end
 puts 'We have lift off!'
+
