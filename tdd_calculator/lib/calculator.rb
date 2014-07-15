8caec281cf8e8d2f920fc8e133083c79930ff2ab
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-07-15 12:56:53 -0600

    completed calculator methods for TDD assignment

---
+++ tdd_calculator/lib/calculator.rb
@@ -0,0 +1,18 @@
+# documentation
+class Calculator
+  def addition(number1, number2)
+    number1 + number2
+  end
+
+  def subtraction(number1, number2)
+    number1 - number2
+  end
+
+  def multiply(number1, number2)
+    number1 * number2
+  end
+
+  def divide(number1, number2)
+    number1 / number2
+  end
+end
